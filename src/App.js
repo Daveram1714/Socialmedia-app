a8cfdced9455da89d13ec19e452c059b3ab0dfb9
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-10-04 05:36:02 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './Header'
+import Nav from './Nav'
+import Home from './Home'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import About from './About'
+import Missing from './Missing'
+import Footer from './Footer'
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route />
+      </Routes>
+
+      {/* <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer /> */}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
