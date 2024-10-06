73f9cf1fd98d15cd31c0e888b152a639519cbc7d
Daveram1714 <143275788+Daveram1714@users.noreply.github.com>
2024-10-06 10:27:51 +0530

    commt

--- src/App.js
+++ src/App.js
@@ -7,13 +7,34 @@ import PostPage from './PostPage'
 import About from './About'
 import Missing from './Missing'
 import Footer from './Footer'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
+import Post from './Post'
 
 const App = () => {
   return (
     <div className='App'>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link> </li>
+            <li><Link to='/about'>about</Link> </li>
+            <li><Link to='/newpost'>new post</Link></li>
+            <li><Link to= '/postpage'>post page</Link> </li>
+
+          </ul>
+
+        </nav>
+
+
       <Routes>
-        <Route />
+        <Route path='*' element ={<Missing />} />
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      <Route path='/postpage'> 
+        <Route index element={<PostPage/>}/>
+        <Route path=':id' element = {<Post />} />
+      </Route>
+        <Route path='/newpost' element={<NewPost/>}/>
+      
       </Routes>
 
       {/* <Header />
--- src/PostPage.js
+++ src/PostPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PostPage = () => {
   return (
     <main>
         <h1>Main</h1>
+        <Link to = '/postpage/1'>Post 1</Link> <br/>
+        <Link to='/postpage/2'>Post 2</Link> <br/>
+        <Link to ='/postpage/3'>Post 3</Link>
     </main>
 )
 }
